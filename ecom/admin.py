cf7de722eac8b646c797a21487deafac03805dc0
AnonymousER45 <82809274+AnonymousER45@users.noreply.github.com>
2021-05-12 00:07:23 +0530

    web app remove

--- ecom/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-# Register your models here.
-
-from .models import Product,Category,subCategory
-admin.site.register(Product),
-admin.site.register(Category),
-admin.site.register(subCategory)
