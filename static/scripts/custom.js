8f9b410bf52c3f6edbfe466b17604986a491baa3
Shaikh Danish <32537559+ShaikhDanish04@users.noreply.github.com>
2021-10-12 01:29:39 +0530

    update

--- static/scripts/custom.js
+++ static/scripts/custom.js
@@ -394,17 +394,17 @@ var items = $(".filled-orders .fo-item");
 
     items.slice(perPage).hide();
 
-    $('#pagination-container').pagination({
-        items: numItems,
-        itemsOnPage: perPage,
-        prevText: "&laquo;",
-        nextText: "&raquo;",
-        onPageClick: function (pageNumber) {
-            var showFrom = perPage * (pageNumber - 1);
-            var showTo = showFrom + perPage;
-            items.hide().slice(showFrom, showTo).show();
-        }
-    });
+    // $('#pagination-container').pagination({
+    //     items: numItems,
+    //     itemsOnPage: perPage,
+    //     prevText: "&laquo;",
+    //     nextText: "&raquo;",
+    //     onPageClick: function (pageNumber) {
+    //         var showFrom = perPage * (pageNumber - 1);
+    //         var showTo = showFrom + perPage;
+    //         items.hide().slice(showFrom, showTo).show();
+    //     }
+    // });
 });
 
 // Gift Modal
